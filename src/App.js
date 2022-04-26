0e66f6da44503a330d7a6c91edafbb583495b7c6
D1eku <dieku25ikari@gmail.com>
2022-04-26 15:28:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import {Button} from "@mui/material";
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
+import {DataHandlerConfig} from "../src/model/util/config/DataHandlerConfig";
+import {ExperimentTwitter} from "./model/environments/twitter/ExperimentTwitter";
 
 function App() {
+
+    const repetitions : number = 3;
+    const name: string = "test experiment twitter"
+    const description: string = "test description"
+    const dataHandlerConfig: DataHandlerConfig = new DataHandlerConfig(name, true, true);
+
+    const startExperiment = () => {
+        let experimentTwitter : ExperimentTwitter = new ExperimentTwitter(repetitions, name, description, dataHandlerConfig);
+        experimentTwitter.run();
+    }
 
     return (
         <div className="App">
