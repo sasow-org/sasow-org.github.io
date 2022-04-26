b0619ff77e1813390f167c4598bc9dc91fbc94d0
D1eku <dieku25ikari@gmail.com>
2022-04-26 15:17:39 -0400

    add mui icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@mui/material";
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hola soy el D1ekeu</h1>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+          <header className="App-header">
+              <Button variant="contained"><PlayCircleFilledWhiteIcon/></Button>
+              <h1>Hola soy el D1ekeu</h1>
+          </header>
+        </div>
+    );
 }
 
 export default App;
