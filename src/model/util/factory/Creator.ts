925f26861fe8d46684221a26e74bd2704b30f6d1
D1eku <dieku25ikari@gmail.com>
2022-04-26 12:58:46 -0400

    run configurations

---
+++ src/model/util/factory/Creator.ts
@@ -0,0 +1,8 @@
+import {Product} from "./Product";
+
+export abstract class Creator {
+
+    public abstract someOperation() : void ;
+    public abstract createProduct(fun:Function) : Product;
+
+}
