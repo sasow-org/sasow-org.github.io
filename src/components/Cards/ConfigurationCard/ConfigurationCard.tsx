b0d6fd60f5ecba993989e7b2d0bcef25d74e28b8
D1eku <dieku25ikari@gmail.com>
2022-05-06 13:55:52 -0400

    Update ConfigurationCard.tsx

--- src/components/Cards/ConfigurationCard/ConfigurationCard.tsx
+++ src/components/Cards/ConfigurationCard/ConfigurationCard.tsx
@@ -4,6 +4,8 @@ import {
     Grid,
 } from "@mui/material";
 import AgentConfigsTable from "../../Table/AgentConfigsTable";
+import ModalEditAgentConfig from "../../Modals/ModalEditAgentConfig";
+import ModalNewAgentConfig from "../../Modals/ModalNewAgentConfig";
 
 export default function ConfigurationCard(props) {
 
@@ -13,22 +15,16 @@ export default function ConfigurationCard(props) {
         padding:"15px",
     }} >
         <Grid container spacing={1}>
-            <Grid item xs={12}>
+            <Grid item xs={8}>
                 <h1>Configuration Card</h1>
+            </Grid>
+            <Grid item xs={4}>
+                <ModalNewAgentConfig experimentConfig={props.experimentConfig}/>
             </Grid>
             <Grid item xs={12}>
                 <AgentConfigsTable experimentConfig={props.experimentConfig} />
             </Grid>
-            <Grid item xs={6}>
-                <Button fullWidth variant="outlined">
-                    Add Agent
-                </Button>
-            </Grid>
-            <Grid item xs={6}>
-                <Button fullWidth color={"error"} variant="contained" >
-                    Delete Agent
-                </Button>
-            </Grid>
+
         </Grid>
     </Card>
 }
