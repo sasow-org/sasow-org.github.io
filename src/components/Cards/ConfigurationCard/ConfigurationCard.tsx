5bb484cb87d22de1d9105d95df2cd3c4f1260765
D1eku <dieku25ikari@gmail.com>
2022-05-06 13:45:26 -0400

    feat: created table

--- src/components/Cards/ConfigurationCard/ConfigurationCard.tsx
+++ src/components/Cards/ConfigurationCard/ConfigurationCard.tsx
@@ -2,73 +2,22 @@ import {
     Button,
     Card,
     Grid,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow
 } from "@mui/material";
+import AgentConfigsTable from "../../Table/AgentConfigsTable";
 
 export default function ConfigurationCard(props) {
-    function createData(
-        name: string,
-        calories: number,
-        fat: number,
-        carbs: number,
-        protein: number,
-    ) {
-        return { name, calories, fat, carbs, protein };
-    }
 
-    const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-    ];
+    console.log(props.experimentConfig)
 
     return <Card style={{
-        backgroundColor: "whitesmoke",
+        padding:"15px",
     }} >
         <Grid container spacing={1}>
             <Grid item xs={12}>
                 <h1>Configuration Card</h1>
             </Grid>
             <Grid item xs={12}>
-                <Table>
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Dessert (100g serving)</TableCell>
-                                    <TableCell align="right">Calories</TableCell>
-                                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {rows.map((row) => (
-                                    <TableRow
-                                        key={row.name}
-                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                    >
-                                        <TableCell component="th" scope="row">
-                                            {row.name}
-                                        </TableCell>
-                                        <TableCell align="right">{row.calories}</TableCell>
-                                        <TableCell align="right">{row.fat}</TableCell>
-                                        <TableCell align="right">{row.carbs}</TableCell>
-                                        <TableCell align="right">{row.protein}</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Table>
+                <AgentConfigsTable experimentConfig={props.experimentConfig} />
             </Grid>
             <Grid item xs={6}>
                 <Button fullWidth variant="outlined">
---
+++ src/components/Table/AgentConfigsTable.tsx
@@ -0,0 +1,86 @@
+import {
+    Button,
+    FormControlLabel,
+    Paper, Switch,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import React, {useState} from "react";
+import ModalEditAgentConfig from "../Modals/ModalEditAgentConfig";
+
+export default function AgentConfigsTable(props) {
+
+    const agentsConfig = props.experimentConfig.agentsConfigs;
+
+    let states = [];
+    agentsConfig.map(ag => {
+        states.push(ag.isSeed)
+    })
+    const [checkedSeedList, setCheckedSeedList] = useState(states);
+
+    console.log(agentsConfig)
+
+    const handleSeedAgent = (event: React.ChangeEvent<HTMLInputElement>, index) => {
+        //console.log("event.target.checked: ",event.target.checked)
+        //console.log("pre sett: ",props.experimentConfig.agentsConfigs[index].isSeed, " index: ", index)
+        //console.log("states pre set: ", states)
+
+        props.experimentConfig.agentsConfigs[index].isSeed = event.target.checked;
+        const statesCopy = []
+        states.map(state => {
+            statesCopy.push(state)
+        })
+        statesCopy[index] = event.target.checked;
+        setCheckedSeedList(statesCopy)
+        //console.log("post sett: ",props.experimentConfig.agentsConfigs[index].isSeed, " index: ", index)
+
+    }
+
+    return (
+        <Table>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Config Name</TableCell>
+                            <TableCell >Percentage Agent</TableCell>
+                            <TableCell >isSeed</TableCell>
+                            <TableCell> Edit </TableCell>
+                            <TableCell >Delete</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {agentsConfig.map((agentConfig, i) => (
+                            <TableRow
+                                key={agentConfig.configName}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {agentConfig.configName}
+                                </TableCell>
+                                <TableCell >{agentConfig.quantityAgent}</TableCell>
+                                <TableCell>
+                                    <Switch checked={checkedSeedList[i]}  onChange={(e) => handleSeedAgent(e,i)}/>
+                                </TableCell>
+                                <TableCell >
+                                    <ModalEditAgentConfig agentConfig={agentConfig}/>
+                                </TableCell>
+                                <TableCell >
+                                    <Button>
+                                        <DeleteIcon/>
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Table>
+    );
+}
