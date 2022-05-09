10aba24cd66e253c7484f828ce68cad4baf03c32
D1eku <dieku25ikari@gmail.com>
2022-05-09 06:35:14 -0400

    refacftoring to buttonstart route

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import StartButton from "./StartButton";
+import StartButton from "./Buttons/StartButton";
 import {createTheme, Menu, MenuItem, Stack, ThemeProvider} from "@mui/material";
 import {useState} from "react";
 
