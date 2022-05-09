3811386d51b103ffad964a95df702a5e0a9d6e87
D1eku <dieku25ikari@gmail.com>
2022-05-09 06:37:12 -0400

    Create ModalNewAgentConfig.tsx

---
+++ src/components/Modals/ModalNewAgentConfig.tsx
@@ -0,0 +1,47 @@
+
+import React, {useState} from "react";
+import {Box, Button, Modal, Typography} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function ModalNewAgentConfig(props) {
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+
+    return (
+        <div>
+            <Button onClick={handleOpen}>
+                <AddIcon/>
+            </Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Edit Agent Config
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                </Box>
+            </Modal>
+        </div>
+    );
+}
