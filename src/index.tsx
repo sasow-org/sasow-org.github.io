e9224ca259dd434c25f4943b65ae324fae8bbc7e
D1eku <dieku25ikari@gmail.com>
2022-05-10 18:38:25 -0400

    trying to deploy

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+process.env.CI = "";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
