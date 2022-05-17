ec4676beb32cb9af3fdb229747a97913b40f8230
D1eku <dieku25ikari@gmail.com>
2022-05-17 13:44:26 -0400

    Create NetworkSelector.tsx

---
+++ src/components/TextField/NetworkSelector.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import {TextField} from "@mui/material";
+import {useState} from "react";
+
+const networkOptions = [
+    {
+        value: "TwitterConfig",
+        label: "Twitter"
+    },
+    {
+        value: "FacebookConfig",
+        label: "Facebook"
+    }
+]
+
+export default function NetworkSelector(props) {
+
+    const [network, setNetwork] = useState(props.experimentType)
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNetwork(event.target.value)
+    }
+
+    return(
+        <TextField
+            id="outlined-select-currency-native"
+            select
+            label="Network to Simulate"
+            value={network}
+            onChange={handleChange}
+            SelectProps={{
+                native: true,
+            }}
+            helperText="Please select your Network"
+            variant="filled"
+            fullWidth
+        >
+            {networkOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                    {option.label}
+                </option>
+            ))}
+        </TextField>
+    )
+}
