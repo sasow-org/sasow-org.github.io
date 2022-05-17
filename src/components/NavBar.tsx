57ed1be2cdd89b652d2f2446f7f3106732b43b32
D1eku <dieku25ikari@gmail.com>
2022-05-17 13:31:10 -0400

    feat: update navbar now have the correct style
    is necessary add funcionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,8 +6,9 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import StartButton from "./Buttons/StartButton";
-import {createTheme, Menu, MenuItem, Stack, ThemeProvider} from "@mui/material";
+import {Button, Container, createTheme, Menu, MenuItem, Stack, ThemeProvider} from "@mui/material";
 import {useState} from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const pages = ['New Experiment', 'Load Experiment', 'Save Experiment'];
 
@@ -31,7 +32,8 @@ export default function NavBar(props) {
         setAnchorElNav(event.currentTarget);
     };
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElUser(event.currentTarget);
+        window.location.href = "https://github.com/sasow-org/sasow-ts/"
+        //setAnchorElUser(event.currentTarget);
     };
 
     const handleCloseNavMenu = () => {
@@ -42,63 +44,111 @@ export default function NavBar(props) {
         setAnchorElUser(null);
     };
 
-    function appBarLabel(label: string) {
+    function appBarLabelFullWidth(label: string) {
         return (
-            <Toolbar>
-                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                    <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleOpenNavMenu}
-                        color="inherit"
-                    >
-                        <MenuIcon />
+            <Container>
+                <Toolbar disableGutters>
+                    <IconButton onClick={handleOpenUserMenu} sx={{ display:{ xs:"none", md: "flex"}, mr:1 }}>
+                        <GitHubIcon />
                     </IconButton>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorElNav}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                        open={Boolean(anchorElNav)}
-                        onClose={handleCloseNavMenu}
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
                         sx={{
-                            display: { xs: 'block', md: 'none' },
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
                         }}
                     >
+                        {label}
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <IconButton onClick={handleOpenUserMenu} sx={{ display:{ xs:"flex", md: "none"}, mr:1 }}>
+                        <GitHubIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        {label}
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                         {pages.map((page) => (
-                            <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                <Typography textAlign="center">{page}</Typography>
-                            </MenuItem>
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
                         ))}
-                    </Menu>
-                </Box>
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="div"
-                    sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-                >
-                    {label}
-                </Typography>
-                <StartButton experimentConfig={props.experimentConfig}/>
-            </Toolbar>
-        );
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <StartButton experimentConfig={props.experimentConfig}/>
+                    </Box>
+                </Toolbar>
+            </Container>)
     }
 
     return (
         <Stack spacing={2} sx={{ flexGrow: 1 }}>
             <ThemeProvider theme={darkTheme}>
                 <AppBar position="static" color="primary">
-                    {appBarLabel('SASOW-TS')}
+                    {appBarLabelFullWidth('SASOW-TS')}
                 </AppBar>
             </ThemeProvider>
         </Stack>
